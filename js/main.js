db2ddc1048810dea38297d9509bcaa83d52f32ca
korneliaflorkowka@gmail.com <106426689+Korflo@users.noreply.github.com>
2023-02-07 20:17:52 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Stwórz plik "main.js".
+
+// Zdeklaruj zmienną o dowolnej nazwie i przypisz do niej tablicę składającą się z 5 liczb.
+
+// Napisz funkcję, która przyjmuje tablicę jako parametr. Funkcja ma zwracać pierwszy element tablicy.
+// Wynik działania funkcji wyświetl w konsoli.
+
+// Plik wgraj do zdalnego repozytorium i podeślij link do sprawdzenia.
+
+
+
+
+let array = [1, 2, 3, 4, 5];
+
+function first (array){
+
+    console.log(array);
+}
+
+first(array[0]);
